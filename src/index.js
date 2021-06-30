eab5b24b2610877d0fb15a56ad95e4f0f6bbe0b3
Danyl Fernandes <androidan1304@gmail.com>
2021-06-30 14:03:04 -0700

    :lipstick: enable graphql playground in prod

--- src/index.js
+++ src/index.js
@@ -30,7 +30,9 @@ const resolvers = {
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    introspection: true,
+    playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
