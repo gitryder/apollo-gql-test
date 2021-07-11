20beb82d5c1c2d111f3027edd72091b7b082e905
Danyl Fernandes <androidan1304@gmail.com>
2021-07-11 11:12:50 -0700

    Replace drinks_db data with thinkread data

--- src/index.js
+++ src/index.js
@@ -1,70 +1,136 @@
 const { ApolloServer } = require('apollo-server');
-const { PrismaClient } = require('@prisma/client')
-const Constants = require('./constants.js');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 const typeDefs = `
     type Query {
         info: String!
-        drinkCount: Int!
-        drinks: [Drink!]!
-        drink(id: Int!): Drink
+        userCount: Int!
+        users(limit: Int): [User!]!
+        user(id: String!): User!
+        postCount: Int!
+        posts(limit: Int): [Post!]!
+        post(id: String!): Post!
     }
 
-    type Mutation {
-        drink(
-            id: ID!
-            name: String!
-            description: String!
-            prepTime: Int!
-            hasAlcohol: Boolean!
-            image: String!
-        ): Drink!
+    type User {
+        id: String!    
+        name: String!    
+        email: String!    
+        profile_picture_url: String!    
+        short_desc: String!    
+        long_desc: String!    
+        social_linked_in: String!   
+        social_instagram: String!    
+        social_twitter: String!
+        comments: [Comment!]!
+        likes: [Like!]!
+      }
+
+    type Post {
+        id: String!
+        title: String!
+        body_text: String!
+        body_html: String!
+        created_by: String!
+        created_on: String!
+        comments: [Comment!]!
+        likes: [Like!]!
     }
 
-    type Drink {
-        id: ID!
-        name: String!
-        description: String!
-        prepTime: Int!
-        hasAlcohol: Boolean!
-        image: String!
+    type Comment {
+        user_id: String 
+        post: Post
+        post_id: String
+        title: String
+        body: String
+        timestamp: String
+    }
+
+    type Like {
+        user_id: String!
+        post_id: String!
+        timestamp: String!
     }
 `;
 
 const resolvers = {
     Query: {
-        info: () => "A GraphQL API running on Apollo server built by dan",
-        drinks: async () => {
-            return await prisma.drink.findMany();
-        },
-        drinkCount: async () => {
-            const rawDrinkCount = await prisma.$queryRaw('SELECT COUNT(id) AS id FROM drinks');
-            return rawDrinkCount[Constants.ARRAY_FIRST_ELEMENT].id;
-        },
-        drink: async (parent, args) => {
-            return await prisma.drink.findUnique({
-                where: {
-                    id: args.id
+        info: () => "Thinkread Provisional API v0.2",
+        userCount: async () => {
+            const totalUserCount = await prisma.user.count({
+                select: {
+                    _all: true
                 }
             });
-        }
-    },
-    Mutation: {
-        drink: async (parent, args) => {  
-            const drink = {
-                id: args.id,
-                name: args.name,
-                description: args.description,
-                prepTime: args.prepTime,
-                hasAlcohol: args.hasAlcohol,
-                image: args.image
-            };
-            await prisma.drink.create({ 
-                data: drink
+            return totalUserCount._all;
+        },
+        users: async (parent, args) => {
+            if (!args.limit) {
+                return await prisma.user.findMany({
+                    include: {
+                        comments: true,
+                        likes: true
+                    }
+                });
+            } else {
+                return await prisma.user.findMany({
+                    take: args.limit,
+                    include: {
+                        comments: true,
+                        likes: true
+                    }
+                });
+            }
+        },
+        user: async (parent, args) => {
+            return await prisma.user.findUnique({
+                where: {
+                    id: args.id
+                },
+                include: {
+                    comments: true,
+                    likes: true
+                }
             });
-            return drink;
+        },
+        postCount: async () => {
+            const totalPostCount = await prisma.post.count({
+                select: {
+                    _all: true
+                }
+            });
+            return totalPostCount._all;
+        },
+        posts: async (parent, args) => {
+            if (!args.limit) {
+                return await prisma.post.findMany({
+                    include: {
+                        comments: true,
+                        likes: true
+                    }
+                });
+            } else {
+                return await prisma.post.findMany({
+                    take: args.limit,
+                    include: {
+                        comments: true,
+                        likes: true
+                    }
+                });
+            }
+        },
+        post: async (parent, args) => {
+            return await prisma.post.findUnique({
+                where: {
+                    id: args.id
+                },
+                include: {
+                    comments: true,
+                    likes: true
+                }
+            });
         }
     }
 }
